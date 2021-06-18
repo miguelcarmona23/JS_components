66d04e8db7bb838c032556492b45fc1283f92346
Miguel Carmona <miguelcarmona996@gmail.com>
2021-06-18 11:08:17 -0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "YOU DID IT! CONGRATULATIONS! WORLDS BEST CUP OF COFFEE!"
+}
